36848214a4ed31dbd37c719310da6dfa0f795488
Robert Apikyan <robert.apikyan@volo.global>
2024-06-03 09:03:37 +0400

    "0.7.4"

--- build.gradle.kts
+++ build.gradle.kts
@@ -419,41 +419,48 @@ task<Exec>(PUSH_TO_REPO_TASK_NAME) {
 	val xcFrameworkDirectory = "${project.projectDir}/build/XCFrameworks/release/"
 	val remoteUrl = "https://github.com/RobertApikyan/RENTESTING.git"
 	workingDir = File(xcFrameworkDirectory)
-	// Initialize Git repository
+	
+//	commandLine("sh","${project.projectDir}/publish.sh")
+	
+//	 Initialize Git repository
 	println("Initializing Git repository...")
 	commandLine("git", "init")
-	
+
 	doLast {
-		
-			
-			// Add remote repository
-//			println("Adding remote repository...")
-//			exec {
-//				commandLine("git", "remote", "add", "sdk", remoteUrl)
-//			}
-			
-			// Add all files
-			println("Adding all files...")
-			exec {
-				commandLine("git", "add", ".")
-			}
-			
-			// Commit changes
-			println("Committing changes...")
-			exec {
-				commandLine("git", "commit", "-m", "\"$PUBLISH_VERSION\"")
-			}
-			
-			// Create tag
-			println("Creating tag...")
-			exec {
-				commandLine("git", "tag", PUBLISH_VERSION)
-			}
-			
-			// Push tag to remote repository
-			println("Pushing tag to remote repository...")
-			exec {
-				commandLine("git", "push", "--force", "sdk", PUBLISH_VERSION)
-			}
+
+
+		// Add remote repository
+		println("Adding remote repository...")
+		exec {
+			commandLine("git", "remote", "add", "sdk", remoteUrl)
+			isIgnoreExitValue = true
+		}
+
+		// Add all files
+		println("Adding all files...")
+		exec {
+			commandLine("git", "add", ".")
+			isIgnoreExitValue = true
+		}
+
+		// Commit changes
+		println("Committing changes...")
+		exec {
+			commandLine("git", "commit", "-m", "\"$PUBLISH_VERSION\"")
+			isIgnoreExitValue = true
+		}
+		// Create tag
+		println("Creating tag...")
+		exec {
+			commandLine("git", "tag", PUBLISH_VERSION)
+			isIgnoreExitValue = true
+		}
+
+		// Push tag to remote repository
+		println("Pushing tag to remote repository...")
+		exec {
+			commandLine("git", "push", "--force", "sdk", PUBLISH_VERSION)
+			isIgnoreExitValue = true
+		}
 	}
 }
